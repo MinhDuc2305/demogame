fde1d86a51e9d7cb5dfdddab892930bb1b9d093f
MinhDuc2305 <bit230109@st.cmcu.edu.vn>
2026-02-08 20:35:07 +0700

    demo part4

--- Assets/scripts/Bullet.cs
+++ Assets/scripts/Bullet.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float flySpeed;
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var enemy = collision.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
